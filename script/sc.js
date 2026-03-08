bf88a9a594d2d1a1fcb2a909a49074cfe7d46373
iftesham82@gmail.com <Iftesham777>
2026-03-09 00:50:45 +0600

    first commit

---
+++ script/sc.js
@@ -0,0 +1,12 @@
+document.getElementById("login-btn").addEventListener("click", function () {
+
+    const username = document.getElementById("input-number").value;
+    const password = document.getElementById("input-pin").value;
+
+    if (username === "admin" && password === "admin123") {
+        window.location.href = "home.html";
+    } else {
+        alert("Wrong username or password");
+    }
+
+});
